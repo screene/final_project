c54d62e4dd8e3e487cb64270866fd09d3e64e575
screene <47026265+screene@users.noreply.github.com>
2020-03-14 20:50:08 +0300

    Update base_page.py
    change invalid on right
    add check icon

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,8 +14,12 @@ class BasePage:
         self.browser.get(self.url)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented,"\
+                                                                     "probably unauthorised user"
 
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
@@ -40,5 +44,5 @@ class BasePage:
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
-
         return True
+
