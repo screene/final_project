975641a1559e6e5f1614ca52dc43bb759a71dc79
screene <47026265+screene@users.noreply.github.com>
2020-03-14 17:45:23 +0300

    Update locators.py
    add locators for BasketPage

--- pages/locators.py
+++ pages/locators.py
@@ -17,3 +17,8 @@ class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
 
+class BasketPageLocators:
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a")
+    BASKET_HAVE_PRODUCT = (By.CSS_SELECTOR, ".col-sm-6.h3")
+    BASKET_IS_EMPTY = (By.ID,"#content_inner")
+
