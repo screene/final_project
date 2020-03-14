db1b27381bd7a05df27d21c858723859957458e7
screene <47026265+screene@users.noreply.github.com>
2020-03-14 20:50:43 +0300

    Update locators.py
    delete invalid selector
    add user icon selector

--- pages/locators.py
+++ pages/locators.py
@@ -5,9 +5,15 @@ class MainPageLocators:
 
 
 class LoginPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_URL = (By.CSS_SELECTOR, "div.container-fluid.page > div > ul > li.active")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
+
 
 class ProductPageLocators:
     ADD_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-add-to-basket")
@@ -15,7 +21,7 @@ class ProductPageLocators:
 
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators:
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group > a")
