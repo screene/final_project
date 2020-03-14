d3a62c76e54c013bcbf83216106afd30dfe4e6da
screene <47026265+screene@users.noreply.github.com>
2020-03-14 16:39:02 +0300

    Create product_page.py

---
+++ pages/product_page.py
@@ -0,0 +1,31 @@
+from selenium.common.exceptions import NoAlertPresentException
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import math
+
+class ProductPage(BasePage):
+    def add_in_basket(self):
+        link = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        link.click()
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappear, but should be"
