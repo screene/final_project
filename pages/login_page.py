ef4ef9991a6ffb21bed982de168daf42e03a8907
screene <47026265+screene@users.noreply.github.com>
2020-03-14 20:51:17 +0300

    Update login_page.py
    delete comments
    add register new user
    add assert go to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,14 +8,21 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
+    def can_go_to_login_page(self):
+        link = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
+        assert link.click()
+
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, "This page is not login"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register from is not present"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD_CONFIRM).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON).click()
