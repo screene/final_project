97d26ff18861843a498569af7f3bf29d66c56348
screene <47026265+screene@users.noreply.github.com>
2020-03-14 20:52:38 +0300

    Update product_page.py
    change add in basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,8 +5,7 @@ import math
 
 class ProductPage(BasePage):
     def add_in_basket(self):
-        link = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
-        link.click()
+        self.browser.find_element(*ProductPageLocators.ADD_BUTTON).click()
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
@@ -25,6 +24,9 @@ class ProductPage(BasePage):
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
             "Success message is presented, but should not be"
+
+    def should_be_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not presented"
 
     def should_disappeared_message(self):
         assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
